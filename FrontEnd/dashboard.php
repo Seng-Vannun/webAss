b7f5874570848e73237bbe54560aa9480e2f1c8e
Seng-Vannun <vannunomega2@gmail.com>
2023-02-10 18:10:02 +0700

    Finish dash Board

--- FrontEnd/dashboard.php
+++ FrontEnd/dashboard.php
@@ -399,7 +399,7 @@ while($StudentStatus=mysqli_fetch_assoc($StudentStatusQuery)){
                                       <a class="dropdown-item" href="javascript:void(0);"
                                       ><i class="bx bx-edit-alt me-1"></i> Edit</a
                                       >
-                                      <a class="dropdown-item" href="javascript:void(0);"
+                                      <a class="dropdown-item" href="../BackEnd/"
                                       ><i class="bx bx-trash me-1"></i> Delete</a
                                       >
                                   </div>
