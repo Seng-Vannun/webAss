3b537b7a95b1d89ab7e2bce40fe7112f41ae8b6c
Seng-Vannun <vannunomega2@gmail.com>
2023-03-14 18:44:14 +0700

    Fixing Resgister validation

--- BackEnd/UsersInput.php
+++ BackEnd/UsersInput.php
@@ -1,56 +1,61 @@
 <?php
 require_once '../BackEnd/db.php';
-echo "you here";
 if (isset($_POST['Submit'])) {
-    $qury="select * from tblusers";
-    $ResultUser=mysqli_query($conn,$qury);
+    $qury = "select * from tblusers";
+    $ResultUser = mysqli_query($conn, $qury);
     $username = $_POST['username'];
-    $password=$_POST['password'];
-    $confirm=$_POST['confirm'];
-    $name=$_POST['name'];
-    $roleid=$_POST['roleid'];
-    $hashPassword=null;
-    $photo=null;
-    while ($check=mysqli_fetch_assoc($ResultUser))
-    {
-        if($check['username']==$username){
-           header("Location:../FrontEnd/register.php?msg= User Name Aready Exist");
+    $password = $_POST['password'];
+    $confirm = $_POST['confirm'];
+    $name = $_POST['name'];
+    $roleid = $_POST['roleid'];
+    $hashPassword = null;
+    $photo = null;
+    $msg=null;
+    if (empty($username)) {
+        $msg="Please Input User Name";
+    }
+    while ($check = mysqli_fetch_assoc($ResultUser)) {
+        echo $check['username'];
+        if ($check['username'] == $username) {
+            $msg="Please UserName Already Exit";
             break;
         }
     }
-    if($password==$confirm){
-        $hashPassword=md5($password);
-    }
-    else
+    if (empty($password))
     {
-    header("Location:../FrontEnd/register.php?msg=Password is not the Same");
+        $msg="Please Input Password";
     }
-    if($_FILES["img"]["size"]==null)
-    {
-        header("Location:../FrontEnd/register.php?msg=No Profile Picture");
+    else if ($password == $confirm) {
+        $hashPassword = md5($password);
     }
-    else
-    {
-        if(move_uploaded_file($_FILES["img"]["tmp_name"],"../BackEnd/images/".date("Ymdhis").$_FILES["img"]["name"]))
-        {
-            $photo= date("Ymdhis").$_FILES["img"]["name"];
+    else {
+        $msg="Your Password is not the Same";
+    }
+    if (empty($name)) {
+        $msg="Please Input Name";
+    }
+    if (empty($roleid)) {
+        $msg="Please Select Role";
+    }
+    if (empty($_FILES["img"]["size"])) {
+        $msg="Please Please Input Profile Picture";
+    } else {
+        if (move_uploaded_file($_FILES["img"]["tmp_name"], "../BackEnd/images/" . date("Ymdhis") . $_FILES["img"]["name"])) {
+            $photo = date("Ymdhis") . $_FILES["img"]["name"];
+        }
+
+    }
+    if(empty($msg)){
+        $result = "insert into `tblusers` (`username`,`password`,`roleid`,`Name`,`Image`) values ('$username','$hashPassword','$roleid','$name','$photo')";
+        if (mysqli_query($conn, $result)) {
+            header("Location: ../FrontEnd/dashboard.php?msg=Account Have Been Created");
+        } else {
+            echo "Error: " . $sql . ":-" . mysqli_error($conn);
         }
     }
-    if($name==null)
-    {
-        header("Location:../FrontEnd/register.php?msg=Input Your Name");
-    }
-    if($roleid==null)
-    {
-        header("Location:../FrontEnd/register.php?msg=Please Pick Role");
+    else{
+        header("Location:../FrontEnd/register.php?msg=".$msg);
     }
 
-    $result="insert into `tblusers` (`username`,`password`,`roleid`,`Name`,`Image`) values ('$username','$hashPassword','$roleid','$name','$photo')";
-  if (mysqli_query($conn, $result)) {
-      header("Location: ../FrontEnd/dashboard.php?msg=Account Have Been Created");
- } else {
-    echo "Error: " . $sql . ":-" . mysqli_error($conn);
- }
- mysqli_close($conn);
 }
 
